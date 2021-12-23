51539d88baf916c9245d24e3b9fe6cbc2f7cfe61
Facundo <ezequiel-mu@live.com.arm>
2021-12-23 19:05:30 -0300

    Banner superior solo en principal

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -44,40 +44,4 @@
       </div>
     </div>
   </div>
-
-
-    <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-indicators">
-          <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-          <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
-        </div>
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <div class="overlay-image" style="background-image:url(/img/slid1.jpg);"></div>
-            <div class="containerz">
-              <div class="carousel-caption">
-                <h1>Una experiencia Inolvidable</h1>
-              </div>
-            </div>
-          </div>
-
-          <div class="carousel-item" >
-            <div class="overlay-image" style="background-image:url(/img/slid2.jpg);"></div>
-            <div class="containerz">
-              <div class="carousel-caption">
-                <h1>Para disfrutar en Familia</h1>
-
-              </div>
-            </div>
-          </div>
-        </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
-        </button>
-      </div>
 </nav>
